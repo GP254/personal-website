6d9d4ad0c612e82e8889db4544f510222ee0cbf1
Gurkirat Panesar <67485512+GP254@users.noreply.github.com>
2025-01-12 12:17:04 -0500

    logo updates

--- scenes/home/scenes/projects/projects.scene.jsx
+++ scenes/home/scenes/projects/projects.scene.jsx
@@ -19,7 +19,7 @@ const constants = {
     {
       title: 'Crime Analysis of Waterloo Region ArcPy',
       description: "Conducted spatial and  crime analysis of Waterloo Region using ArcPy, automating workflows, and identifying crime trends to support public safety and urban planning.",
-      bgImg: analysis.png,
+      bgImg: gisanalysis.png,
       icons: [
         {
           component: <RiGithubLine fill='#2E77BD' size={20}/>,
@@ -30,7 +30,7 @@ const constants = {
     {
       title: 'Nursing Workforce Spatial Data Analysis',
       description: "Conducted thorough spatial analysis on Canadian nursing workforce data using ArcGIS extracting valuable insights from diverse datasets and visualized trends through data manipulation and visualization techniques.",
-      bgImg: analysis.png,
+      bgImg: gisanalysis.png,
       icons: [
         {
           component: <RiGithubLine fill='#2E77BD' size={20}/>,
@@ -41,7 +41,7 @@ const constants = {
     {
       title: 'Student-Performance-Data-Analysis',
       description: "Conducted data analysis on student performance using Python, encompassing data visualization, statistical summaries, and linear regression models for math and writing scores.",
-      bgImg: analysis.png,
+      bgImg: gisanalysis.png,
       icons: [
         {
           component: <RiGithubLine fill='#2E77BD' size={20}/>,
@@ -53,7 +53,7 @@ const constants = {
       title: 'Data Analysis Portfolio',
       description: "Used ArcGIS Storymap to create a centralized location to display all my Data Science and GIS projects and\n" +
           "experience in greater detail.",
-      bgImg: analysis.png,
+      bgImg: gisanalysis.png,
       icons: [
         {
           component: <MdOpenInNew fill='#2E77BD' size={20}/>,
